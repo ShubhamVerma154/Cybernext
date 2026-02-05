e7a93a4560742d4e1d5ffbe906d3032ed346f026
Shubham Verma <shubhamverma154@gmail.com>
2026-02-05 17:51:18 +0530

    Migrate from @studio-freight/lenis to lenis

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
