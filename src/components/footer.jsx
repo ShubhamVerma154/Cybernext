9961508d4845ac7a99c509bb1a04d81ca81e55b6
Shubham Verma <shubhamverma154@gmail.com>
2026-03-03 14:48:37 +0530

    Fix logo path for Linux build

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer>
+            <div className="containerMain">
+                    <div className="leftFtr">
+                        
+                    </div>
+            </div> 
+        </footer>
+    )
+}
